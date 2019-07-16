36f2da22f706fbcb2caaff871c3c7436baff3b88
kevingair <heyhey18>
2019-07-16 15:37:57 -0700

    added symlinks to the main variables.tf file in all modules

--- Terraform/modules/VPC/variables.tf
+++ Terraform/modules/VPC/variables.tf
@@ -1,9 +1,1 @@
-variable "vpc_name" {
-  description = "VPC name"
-  default     = "opencue-vpc"
-}
-
-variable "auto_create_subnetworks" {
-  default     = "true"
-  description = "false = creates custom VPC, true = automatically creates subnets in each region."
-}
+../../variables.tf
