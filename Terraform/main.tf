3e050506be489bae1d47fa4d59ccf0ca47bed900
Kevin McCartney <kevinmccartney@kevins-air.lan>
2020-04-15 14:24:27 -0700

    added GKE module

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,12 +1,9 @@
-module "opencue_master" {
-   source = "./modules/compute"
-   compute_name = "opencue-master"
+#module "opencue_master" {
+#   source = "./modules/compute"
+#   compute_name = "opencue-master"
+#
+#}
 
-}
-
-module "filestore_create" {
-  source = "./modules/storage"
-  storage_instance_name = "test"
-  capacity_gb = 2000
-  share_name = "content"
+module "GKE" {
+    source = "./modules/GKE"
 }
